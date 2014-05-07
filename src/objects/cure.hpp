2da6a147818bd8ff1bfc296cf2269f7bd385717a
Roy <rikkyroy@gmail.com>
2014-05-07 13:28:59 -0400

    Almost fully implemented Serial Simulation for objects
    just need to modify evolve_virus function more
    
    Signed-off-by: Roy <rikkyroy@gmail.com>

--- src/objects/cure.hpp
+++ src/objects/cure.hpp
@@ -1,22 +1,32 @@
-#include "vaccine.hpp"
+#ifndef CURE_H
+#define CURE_H
+
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-
+#include "person.hpp"
 class Cure {
 
 public:
-	Cure(int dna, std::string name, std::string virus, float strength, int cost);
-	//~cure();
+	Cure(int id, std::string name, std::string virus, int strength);
+	Cure();
+	~Cure();
 
 	void cure(Person person);
+	std::string get_name();
+	std::string get_cured_virus_name();
+	int get_strength();
+	int get_id();
+	void set_id(int value);
+	void set_strength(int value);
+
 private:
-	int dna;
+	int id; // a higher id value is better
 	std::string name;
 	std::string cured_virus;
-	float strength;
-
-	double cost;
+	int strength; 
 
 	int* cured; // list of all array positions of people cured
 };
+
+#endif /* CURE_H*/
